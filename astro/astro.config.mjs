c29af4fa0c9b67c4df9e9b05bf354ae12e4ed75d
Shamil <toshamilgis@gmail.com>
2024-11-27 13:39:54 +0300

    home-page

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,16 +2,16 @@
 import { defineConfig } from "astro/config";
 import paraglide from "@inlang/paraglide-astro";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     locales: ["en", "ru"],
     defaultLocale: "ru",
   },
-  integrations: [
-    paraglide({
-      project: "./project.inlang",
-      outdir: "./src/paraglide",
-    }),
-  ],
+  integrations: [paraglide({
+    project: "./project.inlang",
+    outdir: "./src/paraglide",
+  }), react()],
 });
