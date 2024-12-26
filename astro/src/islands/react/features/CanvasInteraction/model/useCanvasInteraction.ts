dcfd2ff432526748f86de883431cad1a0e5bbee1
Sham <sham@MacBook-Air-Sham.local>
2024-12-27 00:53:59 +0300

    fix-error-with-selecting

--- astro/src/islands/react/features/CanvasInteraction/model/useCanvasInteraction.ts
+++ astro/src/islands/react/features/CanvasInteraction/model/useCanvasInteraction.ts
@@ -66,10 +66,9 @@ export const useCanvasInteraction = (canvas: fabric.Canvas | null) => {
     const onSelection: OnSelect = ({ selected, deselected }) => {
       if (selected.length === 0) return;
       const updatedSelection = Objects.get().map((object) => {
-        return {
-          ...object,
-          isSelected: selected.includes(object.instance),
-        };
+        const newObject = { ...object }
+        newObject.generatorConfiguration.isSelected = selected.includes(object.generatorConfiguration.instance);
+        return newObject;
       });
       Objects.set(updatedSelection);
     };
